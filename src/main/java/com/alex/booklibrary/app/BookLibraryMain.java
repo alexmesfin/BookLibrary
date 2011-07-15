946fa681b8b04cc6f9bf9b321a4cad8e96fc9693
Alex Mesfin <tsssmesfin@comcast.net>
2011-07-14 22:53:11 -0500

    Initial Checkin

---
+++ src/main/java/com/alex/booklibrary/app/BookLibraryMain.java
@@ -0,0 +1,11 @@
+package com.alex.booklibrary.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class BookLibraryMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/alex/booklibrary/app/BookLibraryMainTest.java
@@ -0,0 +1,39 @@
+package com.alex.booklibrary.app;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class BookLibraryMainTest extends TestCase {
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public BookLibraryMainTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(BookLibraryMainTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
